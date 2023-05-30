765754f5303d63c421fc66eccbc0ead7b7a6b256
Zachral <soderberg.danne@gmail.com>
2023-05-30 12:46:22 +0200

    added enum for move direction, rewritten logic

--- snakeMovement.c
+++ snakeMovement.c
@@ -1,11 +1,30 @@
+#include "snakeMovement.h"
+
+movement lastMove; 
 int countiousMovementX(int lastX, int x){
-       if(x > lastX) return x + 1;
-       else if(x < lastX) return x - 1;
-       else return x; 
+       if(x > lastX){
+        lastMove = Snake_right;
+        return x + 1;
+       }else if(x < lastX){
+        lastMove = Snake_left; 
+        return x - 1;
+       }else if (lastMove == Snake_right){ 
+        return x + 1;
+       }else if (lastMove == Snake_left){ 
+        return x - 1;  
+       }
 }
 
 int countiousMovementY(int lastY, int y){
-       if(y > lastY) return y + 1;
-       else if(y < lastY) return y - 1;
-       else return y; 
+        if(y > lastY){
+            lastMove = Snake_down;
+            return y + 1;
+       }else if(y < lastY){
+            lastMove = Snake_Up; 
+            return y - 1;
+       }else if(lastMove == Snake_down){
+            return y + 1; 
+       }else if(lastMove == Snake_Up){
+            return y - 1;  
+       }
 }
--- snakeMovement.h
+++ snakeMovement.h
@@ -1,7 +1,16 @@
 #ifndef __SNAKE_MOVEMENT_H_
 #define __SNAKE_MOVEMENT_H_
 
+typedef enum {
+    Snake_Up,
+    Snake_down,
+    Snake_left,
+    Snake_right
+} movement; 
+
+
 int countiousMovementX(int lastX, int x);
 int countiousMovementY(int lastY, int y); 
+
 
 #endif
